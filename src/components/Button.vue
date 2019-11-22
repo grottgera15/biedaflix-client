bd45236e53a7926acf9bf6a1380402211da571f8
Maksymilian Łakomy <zommer128@gmail.com>
2019-11-22 23:23:26 +0100

    Reworking Player

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+	<button>
+        <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+	name: "Button"
+};
+</script>
+
+<style scoped lang="sass">
+    button
+            letter-spacing: 0.5px
+            font-weight: 700
+            font-family: inherit
+            width: 100%
+            padding: 16px                
+            border: 1px solid #f86356
+            border-radius: 4px
+            color: white
+            background-color: #f86356
+            cursor: pointer
+            transition-duration: 0.5s
+            &:hover
+                box-shadow: 0 2px 10px #00000080
+            &:focus
+                outline: unset !important
+</style>
