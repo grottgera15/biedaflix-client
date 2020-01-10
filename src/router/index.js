6370dbdd4ad69b9af4e305ee19cebd1d1494dc50
Maksymilian Łakomy <zommer128@gmail.com>
2020-01-10 14:31:41 +0100

    Removing obsolete things again

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Registration from '../views/Registration.vue'
-import About from '../views/About.vue'
 import Player from '../views/Player.vue'
 
 Vue.use(VueRouter)
@@ -14,15 +13,6 @@ const routes = [
         path: '/',
         name: 'home',
         component: Home
-    },
-    {
-        path: '/about:sources:pathSubs',
-        name: 'about',
-        component: About,
-        props: {
-            sources: 'SiliconValleyS01E01.mkv',
-            pathSubs: 'SiliconValleyS01E01.json'
-        }
     },
     {
         path: '/login',
