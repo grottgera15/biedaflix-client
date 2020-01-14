e02b10a8cd4bfc52487b20ae6bb78e20d4c640eb
Maksymilian Łakomy <zommer128@gmail.com>
2020-01-14 13:31:59 +0100

    Added vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: { 
+        count: 0,
+        sourceDestination: null
+    },
+    mutations: {
+        increment (state) {
+            state.count++;
+        }
+    }
+})
 
 new Vue({
   router,
