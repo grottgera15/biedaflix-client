94cda46d881cb2f31770d793956a3198081299ef
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-07 20:29:30 +0100

    Redo StateButton.vue

--- src/components/Forms/Buttons/StateButton.vue
+++ src/components/Forms/Buttons/StateButton.vue
@@ -1,84 +1,73 @@
 <template>
-	<div>
-		<div class="state-wrapper" @click="onClick">
-			<div class="state-button">
-				<div
-					class="state"
-					:style="{left: `calc(${position})`, width: `${size}px`, height: `${size}px`, 'margin-top': `${-size/2}px` }"
-				/>
-			</div>
-		</div>
-		<div class="text">
-			<slot />
-		</div>
-	</div>
+    <button @click="onClick" @mouseenter="hover = true" @mouseleave="hover = false" class="button">
+        <div class="button__state" :class="classesBind" />
+    </button>
 </template>
 
 <script>
 export default {
-	name: "StateButton",
-	data: function() {
-		return {
-			state: false
-		};
-	},
-	props: {
-		size: {
-			type: Number,
-			default: 16
-		},
-		margin: {
-			type: Number,
-			default: 2
-		}
+    name: "StateButton",
+    data: function() {
+        return {
+            hover: false
+        };
     },
-    methods: {
-        onClick: function() {
-            this.state = !this.state;
-            this.$emit("click", this.state);
+    props: {
+        value: {
+            type: Boolean,
+            required: true
         }
     },
-	computed: {
-		position: function() {
-			if (!this.state) return `0% - ${this.margin}px`;
-			else return `100% - ${this.size - this.margin}px`;
-		}
-	}
+    methods: {
+        onClick: function(event) {
+            this.$emit("click", event);
+        }
+    },
+    computed: {
+        position: function() {
+            if (!this.state) return `0% - ${this.margin}px`;
+            else return `100% - ${this.size - this.margin}px`;
+        },
+        classesBind: function() {
+            return {
+                'button__state--hover': this.hover,
+                'button__state--left': this.value
+            }
+        }
+    }
 };
 </script>
 
 <style lang="sass" scoped>
-.state-wrapper
-    display: inline-block
-    height: max-content
-    width: min-content
+@import "@styles/variables"
+
+button
+    border: unset
+    background-color: unset
+    outline: unset
     cursor: pointer
-    vertical-align: middle
-    padding: 8px 0px
-    margin-bottom: 6px
+    padding: unset
+    margin: unset
 
-    .state-button
-        position: relative
-        height: 9px
-        width: 24px
-        border-radius: 5px
-        background-color: #9b9b9b
-        cursor: pointer    
+.button
+    width: calc(3em + 2 * 1px)
+    height: calc(1.5em + 2 * 1px)
+    border: 1px solid $bright-dark-color
+    position: relative
 
-    .state
-        position: absolute
-        border-radius: 50%
-        top: 50%
-        background-color: firebrick
+    &__state
+        top: 0
+        left: 0
+        height: 100%
+        width: 1.5em
+        background-color: $second-color
         transition-duration: .2s
+        position: absolute
 
-.text
-    font-size: 10pt
-    letter-spacing: .5px
-    vertical-align: top
-    display: inline-block
-    cursor: default
-    color: #9b9b9b
-    margin-left: 4px
+    &__state--hover
+        background-color: $main-color
+
+    &__state--left
+        left: 1.5em
 
 </style>
