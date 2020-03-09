417c337fe418fce9cada04a1eeb8e3dda6bc9311
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-09 18:47:21 +0100

    Created enums for series and episode statuses

---
+++ src/enums/episodeStatuses.js
@@ -0,0 +1,11 @@
+export default {
+    UNAVAILABLE: {
+        name: "Niedostępny"
+    },
+    NO_SUBTITLES: {
+        name: "Brak napisów"
+    },
+    AVAILABLE: {
+        name: "Dostępny"
+    }
+}
---
+++ src/enums/seriesStatuses.js
@@ -0,0 +1,14 @@
+export default {
+    ANNOUNCED: {
+        name: "Zapowiedziany"
+    },
+    ONGOING: {
+        name: "Trwa"
+    },
+    FINISHED: {
+        name: "Zakończony"
+    },
+    NEW_EPISODES: {
+        name: "Nowe odcinki"
+    }
+}
