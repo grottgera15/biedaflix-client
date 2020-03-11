402d02eb62f94f8fdf0fd74d645182e596cc0497
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-11 16:47:23 +0100

    Fixed auth.js

--- src/auth.js
+++ src/auth.js
@@ -8,48 +8,53 @@ import UserMutations from "@vuexMutations/UserMutations";
 async function login({ login, password }) {
     if (login === undefined || password === undefined)
         throw new ReferenceError("You need to provide login and password to login function!");
-    axios.post(`${process.env.VUE_APP_API_PATH}/login`, { login, password }, {
+
+    const response = await axios.post(`${process.env.VUE_APP_API_PATH}/login`, { login, password }, {
         header: {
             'content-type': 'application/json'
-        }, withCredentials: true
-    }).then(() => {
+        },
+        withCredentials: true,
+        validateStatus: function (status) {
+            return status >= 200 && status <= 401;
+        }
+    });
+
+    if (response.status === 200) {
         updateUser();
-        auth();
+        startTokenRefreshment();
         return true;
-    }).catch(() => {
+    } else {
         return false;
-    })
+    }
 }
 
 async function refreshToken() {
-    axios.post(`${process.env.VUE_APP_API_PATH}/refreshToken`, {}, {
+    const response = await axios.post(`${process.env.VUE_APP_API_PATH}/refreshToken`, {}, {
         withCredentials: true,
-        validateStatus: ((status) => {
+        validateStatus: function (status) {
             return status >= 200 && status <= 401;
-        })
-    }).then(() => {
-        updateUser();
-        auth();
-        return true;
-    }).catch((error) => {
-        Promise.reject(error);
-        return false;
+        }
     });
+    if (response.status === 200) {
+        updateUser();
+        startTokenRefreshment();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function startTokenRefreshment() {
+    setTimeout(async () => {
+        refreshToken();
+    }, cookie.get('jwt_token_expiry') - Date.now() - 6000);
 }
 
 async function auth() {
-    if (!cookie.get('jwt_token') ||
-        !cookie.get('jwt_token_expiry') ||
-        new Date(cookie.get('jwt_token_expiry')) >= Date.now() - 6000) {
-        return await refreshToken();
-    } else {
-        if (!store.getters.user)
-            updateUser();
-        setTimeout(async () => {
-            await refreshToken();
-        }, cookie.get('jwt_token_expiry') - Date.now() - 6000);
-    }
-    return true;
+    if (store.getters.user)
+        return true;
+    const result = await refreshToken();
+    return result;
 }
 
 import UserData from "@classes/UserData";
